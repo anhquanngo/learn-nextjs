918a5e9ab4841fdace68dab3d68647b4e9bb6ad3
quan <anhquan31299@gmail.com>
2021-12-20 14:27:18 +0700

    setup flow

--- pages/api/[...path].ts
+++ pages/api/[...path].ts
@@ -1,6 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 import httpProxy from 'http-proxy';
+import { resolve } from 'path/posix';
+import Cookies from 'cookies';
 
 type Data = {
   name: string
@@ -18,13 +20,26 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  // don't send cookies to API server
-  req.headers.cookie = '';
+  return new Promise((resolve) => {
+    // convert cookies to header to Authoritation
+    const cookies = new Cookies(req, res)
+    const accessToken = cookies.get('access_token')
+    if (accessToken) {
+      req.headers.authorization = `Bearer ${accessToken}`
+    }
 
-  proxy.web(req, res, {
-    target: process.env.API_URL,
-    changeOrigin: true,
-    selfHandleResponse: false
+    // don't send cookies to API server
+    req.headers.cookie = '';
+
+    // /api/students
+    // /https://js-post-api.herrokuapp.com/api/students
+    proxy.web(req, res, {
+      target: process.env.API_URL,
+      changeOrigin: true,
+      selfHandleResponse: false
+    })
+    proxy.once('proxyReq', () => {
+      resolve(true)
+    })
   })
-  res.status(200).json({ name: 'PATH - Match all here' })
 }
---
+++ pages/api/login.ts
@@ -0,0 +1,68 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import httpProxy, { ProxyReqCallback } from 'http-proxy';
+import Cookies from 'cookies'
+
+type Data = {
+    message: string
+}
+
+export const config = {
+    api: {
+        bodyParser: false,
+    },
+}
+
+const proxy = httpProxy.createProxyServer()
+
+export default function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<Data>
+) {
+    if (req.method !== 'POST') {
+        return res.status(404).json({ message: "method not supported" })
+    }
+    return new Promise((resolve) => {
+        // don't send cookies to API server
+        console.log('login request');
+
+        req.headers.cookie = '';
+
+        const handleLoginResponse: ProxyReqCallback = (proxyRes, req, res) => {
+            let body = '';
+            proxyRes.on('data', function (chunk: any) {
+                body += chunk;
+            });
+            proxyRes.on('end', function () {
+                const { accessToken, expireAt } = JSON.parse(body)
+
+                // convert token to cookies
+                const cookies = new Cookies(req, res, { secure: process.env.NODE_ENV === 'development' });
+                cookies.set('accessToken', accessToken, {
+                    httpOnly: true,
+                    sameSite: 'lax',
+                    expires: new Date(expireAt)
+                })
+
+
+                try {
+                    (res as NextApiResponse).status(200).json({ massage: 'login successfully' })
+                } catch (error) {
+                    (res as NextApiResponse).status(500).json({ massage: 'something went wrong' })
+                }
+
+                resolve(true)
+            });
+        }
+
+        proxy.once('proxyReq', handleLoginResponse)
+
+        // /api/students
+        // /https://js-post-api.herrokuapp.com/api/students
+        proxy.web(req, res, {
+            target: process.env.API_URL,
+            changeOrigin: true,
+            selfHandleResponse: true
+        })
+    })
+}
